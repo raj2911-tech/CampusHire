01bb2fc33b2865e4831302de8e6fc0dafb5c563b
raj2911-tech <raj317073b@gmail.com>
2026-01-06 12:36:23 +0530

    user register method completed

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -5,13 +5,38 @@ import collegeModel from '../../models/colleges.js'
 import studentModel from '../../models/students.js';
 import companyModel from '../../models/companies.js';
 
-export const register=async(req, res)=>{
-    res.status(200).json(
-        {
-            success: true,
-            message: "Register controller reached"
+export const register= async(req, res)=>{
+    const {email, password,role}=req.body;
+
+    if(!email || !password || !role){
+        return res.json({success: false, message: 'Missing Details'})
+    }
+
+    if(role!=="STUDENT" && role!=="COLLEGE" && role!=="COMPANY"){
+        return res.json({success: false, message: 'Invalid Role'})
+    }
+
+    try{
+
+        const existingUser = await userModel.findOne({email});
+        
+        if(existingUser){
+            return res.json({success: false, message: "User already exists"});
         }
-    );
+
+        const hashedPassword= await bcrypt.hash(password, 10);
+
+        const user=new userModel({email, password: hashedPassword,role});
+        await user.save();
+
+        return res.json({success:true});
+
+
+    } catch(error){
+        return res.json({success: false, message: error.message})
+
+    }
+
 
 };
 
