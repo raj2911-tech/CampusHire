92303222f5067c2be559b6463a011e6d57e6509c
raj2911-tech <raj317073b@gmail.com>
2026-01-08 16:03:30 +0530

    college module started to develop
    - see profile
    - update profile

--- controllers/college/collegeController.js
+++ controllers/college/collegeController.js
@@ -1,14 +1,23 @@
 import collegeModel from "../../models/colleges.js";
 
-export const getRegisteredColleges = async (req, res) => {
+export const getProfile = async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const colleges = await collegeModel.find({}, { name: 1 });
-    // returns: [{ _id, name }]
+    const profile = await collegeModel.findById(id);
+
+    if (!profile) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found"
+      });
+    }
 
     return res.json({
       success: true,
-      colleges
+      profile
     });
+
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -16,3 +25,41 @@ export const getRegisteredColleges = async (req, res) => {
     });
   }
 };
+
+export const updateProfile = async (req, res) => {
+    const { id } = req.params;
+    const { name, placementOfficer } = req.body;
+  
+    try {
+      if (!name || !placementOfficer) {
+        return res.status(400).json({
+          success: false,
+          message: "Missing college details"
+        });
+      }
+  
+      const result = await collegeModel.updateOne(
+        { _id: id },                 // filter
+        { $set: { name, placementOfficer } } // update
+      );
+  
+      if (result.matchedCount === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "College not found"
+        });
+      }
+  
+      return res.json({
+        success: true,
+        message: "Profile updated successfully"
+      });
+  
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message
+      });
+    }
+  };
+  
--- routes/college/collegeRoutes.js
+++ routes/college/collegeRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getRegisteredColleges } from "../../controllers/college/collegeController.js";
+import { getRegisteredColleges } from "../../services/getCollegeNames.js";
+import {getProfile, updateProfile} from "../../controllers/college/collegeController.js"
 
 const router = express.Router();
 
-// PUBLIC (no auth needed)
-router.get("/public", getRegisteredColleges);
+
+router.get("/public", getRegisteredColleges); // PUBLIC (no auth needed)
+router.get("/profile/:id", getProfile);
+router.post("/profile/update/:id", updateProfile);
+
+
+
+
 
 export default router;
